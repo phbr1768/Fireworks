b62e58586ccec99de4f727f29d205a8825eb6117
Phillip Brown <92740792+phbr1768@users.noreply.github.com>
2025-05-05 16:33:11 -0400

    .

--- firework.gd
+++ firework.gd
@@ -9,21 +9,23 @@ extends RigidBody3D
 
 #timer setups
 @onready var fuse_timer = $FuseTimer
-var fuse_per_dist = 0.5
+var fuse_per_dist = 0.25
 @onready var charge_timer = $ChargeTimer
 @onready var explosion_timer = $ExplosionTimer
 
 #launch acceleration
+var launched = false
 var accel = 50.0
 
 #particle refs
 @onready var spark_particles = $SparkParticles
 @onready var smoke_particles = $SmokeParticles
-@onready var explosion_particles = $ExplosionParticles
+var explosion_particles : GPUParticles3D;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	explosion_particles = get_child(randi_range(-7,-1))
 	pass
 
 func setup():
@@ -32,6 +34,9 @@ func setup():
 	fuse_timer.set_wait_time(planar_dist * fuse_per_dist)
 
 func launch():
+	if launched:
+		return
+	launched = true
 	spark_particles.set_emitting(true)
 	fuse_timer.start()
 
@@ -39,7 +44,6 @@ func _on_fuse_timer_timeout():
 	freeze = false
 	add_constant_force(Vector3.UP * accel)
 	stick.visible = false
-	spark_particles.set_emitting(false)
 	smoke_particles.set_emitting(true)
 	charge_timer.start()
 	pass # Replace with function body.
@@ -47,11 +51,26 @@ func _on_fuse_timer_timeout():
 func _on_charge_timer_timeout():
 	freeze = true
 	constant_force = Vector3.ZERO
+	spark_particles.set_emitting(false)
 	smoke_particles.set_emitting(false)
 	explosion_particles.set_emitting(true)
-	explosion_timer.start()
 	pass # Replace with function body.
 
 func _on_explosion_timer_timeout():
 	queue_free()
+	pass # Replace with function body.
+
+func _on_palm_effect_finished():
+	print("palm finished")
+	explosion_timer.start(5)
+	pass # Replace with function body.
+
+func _on_fish_effect_finished():
+	print("fish finished")
+	explosion_timer.start(5)
+	pass # Replace with function body.
+
+func _on_crackle_effect_finished():
+	print("crackle finished")
+	explosion_timer.start(5)
 	pass # Replace with function body.
--- scripts/movement.gd
+++ scripts/movement.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta):
 	# Add the gravity.
+	if Input.is_action_just_pressed("reset"):
+		position = Vector3(0,0,0)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
